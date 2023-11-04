4a68af3ddeb899a7d49ff4a226d437ad2963b92a
Matt Webb <matt@interconnected.org>
2023-11-04 14:10:31 +0000

    Show overlay

--- web-component/src/components/braggoscope-search/braggoscope-search.tsx
+++ web-component/src/components/braggoscope-search/braggoscope-search.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, State, h } from '@stencil/core';
 
 @Component({
   tag: 'braggoscope-search',
@@ -6,11 +6,31 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class BraggoscopeSearch {
+  @State() show: boolean = false;
+
   render() {
     return (
       <Host>
-        <div>Hello, World!</div>
-        <slot></slot>
+        {!this.show && (
+          <button
+            onClick={() => {
+              this.show = true;
+            }}
+          >
+            <slot></slot>
+          </button>
+        )}
+        {this.show && (
+          <div id="overlay">
+            <div
+              class="absolute top-0 bottom-0 left-0 right-0"
+              onClick={() => {
+                this.show = false;
+              }}
+            />
+            <div class="relative flex flex-col bg-white justify-start items-start gap-6 w-full max-w-md p-6">Search</div>
+          </div>
+        )}
       </Host>
     );
   }
