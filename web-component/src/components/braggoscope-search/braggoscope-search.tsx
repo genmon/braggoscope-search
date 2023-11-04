642c2933b471dd683e620559bad2c2b533991674
Matt Webb <matt@interconnected.org>
2023-11-04 14:24:13 +0000

    Search form

--- web-component/src/components/braggoscope-search/braggoscope-search.tsx
+++ web-component/src/components/braggoscope-search/braggoscope-search.tsx
@@ -7,6 +7,19 @@ import { Component, Host, State, h } from '@stencil/core';
 })
 export class BraggoscopeSearch {
   @State() show: boolean = false;
+  @State() query: string = '';
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // blur the form
+    event.currentTarget.querySelector('input')?.blur();
+    this.show = false;
+    //window.location.href = `https://www.braggoscope.com/search?q=${this.query}`;
+  }
+
+  handleQueryChange(event) {
+    this.query = event.target.value;
+  }
 
   render() {
     return (
@@ -28,7 +41,14 @@ export class BraggoscopeSearch {
                 this.show = false;
               }}
             />
-            <div class="relative flex flex-col bg-white justify-start items-start gap-6 w-full max-w-md p-6">Search</div>
+            <div class="relative flex flex-col justify-start items-start gap-6 w-full max-w-md">
+              <form onSubmit={e => this.handleSubmit(e)} class="w-full flex justify-start items-center gap-1">
+                <input class="grow border border-gray-300 rounded-sm px-2 py-2 w-full" type="text" value={this.query} onChange={e => this.handleQueryChange(e)} />
+                <button class="grow-0 bg-white border border-blue-500 hover:bg-blue-100 text-blue-500 hover:text-blue-700 font-semibold py-2 px-4 rounded-sm" type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
           </div>
         )}
       </Host>
