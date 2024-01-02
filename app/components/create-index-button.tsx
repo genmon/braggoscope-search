85986750965ac99303b26a702436bc3c60d61496
Matt Webb <matt@interconnected.org>
2024-01-02 17:08:30 +0000

    Styling fix

--- app/components/create-index-button.tsx
+++ app/components/create-index-button.tsx
@@ -4,19 +4,22 @@ export default function CreateIndexButton(props: {
   target: number;
   handleClick: () => void;
 }) {
+  const { isDone, progress, target, handleClick } = props;
   return (
     <button
       className="relative border border-blue-500 hover:bg-blue-100 disabled:hover:bg-transparent text-blue-500 disabled:text-black/50 disabled:cursor-wait font-semibold py-2 px-6 rounded-sm"
-      onClick={props.handleClick}
-      disabled={!props.isDone}
+      onClick={handleClick}
+      disabled={!isDone}
     >
-      <progress
-        className="-z-10 absolute top-0 left-0 bottom-0 right-0 w-full h-full progress-unfilled:bg-white progress-filled:bg-blue-300"
-        style={{ WebkitAppearance: "none" }}
-        value={props.progress}
-        max={props.target}
-      />
-      {props.isDone ? "Create Index" : "Working..."}
+      {isDone || (
+        <progress
+          className="-z-10 absolute top-0 left-0 bottom-0 right-0 w-full h-full progress-unfilled:bg-white progress-filled:bg-blue-300"
+          style={{ WebkitAppearance: "none" }}
+          value={progress}
+          max={target}
+        />
+      )}
+      {isDone ? "Create Index" : "Working..."}
     </button>
   );
 }
